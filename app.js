00edb1f46b71dc3e9ae449666ad7f5548c060d59
Dean <dean.l.murray@hotmail.com>
2018-07-31 15:32:44 -0600

    Added Proc File

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+//I think that this line could be axed?
+const {createServer} = require ('http');
+//I think that this line could be axed
+
+
 var express = require('express');
 var app = express();
 
+const normalizePort = port => parseInt(port, 10);
+const PORT = normalizePort(process.env.PORT || 5000);
+
 app.get('/', function(req, res) {
     res.send('Hello World');
-}).listen(3000);
+}).listen(PORT);
 
-console.log("Waiting for requests...");
+//const server = createServer(app);
+
+//server.listen(PORT, err => {
+//    if(err) throw err;
+
+//    console.log('Server started!');
+//})
